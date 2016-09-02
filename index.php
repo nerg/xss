328c9aab08b757114ab6a88f26f0a2015f348f12
Renevey Gilles ZAS <gilles.renevey@zas.admin.ch>
2016-09-02 18:42:28 +0200

    update

--- index.php
+++ index.php
@@ -5,8 +5,21 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
+  <style>
+  table {
+    width: 100%;
+  }
+  td {
+    margin:10px;
+    padding:10px;
+  }
+  </style>
     <div class="container">
     <?php
+      $mysqli = new mysqli("localhost", "root", "", "xss");
+      if ($mysqli->connect_errno) echo "Failed (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+
+
       if ($_GET == null ||
           $_GET['name'] == null ||
           $_GET['address'] == null ||
@@ -14,6 +27,24 @@
     ?>
     <form action="" method="GET">
     <div class="form-group">
+      <br>
+      <h2>Student List</h2><br>
+<table border=1>
+<?php
+$sql = "SELECT name, address, date_of_birth FROM students";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "<tr><td>".$row["name"]."</td><td>".$row["address"]."</td><td>".$row["date_of_birth"]."</td></tr>";
+    }
+} else {
+    echo "0 results";
+}
+?>
+</table>
+      <br>
       <h2>Student Registration</h2><br>
       <label for="name">Name</label>
       <input class="form-control" type="text" value="" name="name"><br>
@@ -23,11 +54,9 @@
       <input class="form-control" type="text" value="" name="date_of_birth"><br>
       <input class="btn btn-default" type="submit" value="Register">
       </div>
-    </form>    
+    </form>
     <?php
       } else {
-        $mysqli = new mysqli("localhost", "root", "", "xss");
-        if ($mysqli->connect_errno) echo "Failed (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 
         // Variables utilisées pour l'injection SQL
         $name          = $_GET['name'];
@@ -58,10 +87,18 @@
 // ENTER
 // name=robert",'',''); delete from students;
 
-CREATE TABLE `students` (
-  `name` varchar(64),
-  `address` varchar(255),
-  `date_of_birth` varchar(10)
-);
+CREATE TABLE IF NOT EXISTS `students` (
+  `name` varchar(64) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `date_of_birth` varchar(10) DEFAULT NULL,
+  UNIQUE KEY `name` (`name`)
+)
+
+INSERT INTO `students` (`name`, `address`, `date_of_birth`) VALUES
+('Nicolas', '12 chemin du Village A', '12.12.2012'),
+('Pierre', '27 chemin du village B', '12.12.2012'),
+('Paul', '27 chemin du village C', '13.12.2012'),
+('Alain', '23 chemin du village D', '14.12.2012');
+
 */
 ?>
